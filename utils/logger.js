6daa8d124fcb6ac21180261568e68a3fdf83e66e
Tube <52958347+OfficialTube@users.noreply.github.com>
2025-10-22 21:44:02 -0400

    Bot Logs role that will be pinged instead of just Tube

--- utils/logger.js
+++ utils/logger.js
@@ -1,5 +1,5 @@
 const BOT_LOG_CHANNEL_ID = "1424584591590555648"; 
-const YOUR_USER_ID = "464597977798017024"; 
+const BOT_LOGS_ROLE = "1430732603731738634"; 
 let clientRef;
 
 function setClient(client) {
@@ -11,7 +11,7 @@ async function logOffline(message) {
   try {
     const channel = await clientRef.channels.fetch(BOT_LOG_CHANNEL_ID);
     if (channel && channel.isTextBased()) {
-      await channel.send(`<@${YOUR_USER_ID}> ${message}`);
+      await channel.send(`<@${BOT_LOGS_ROLE}> ${message}`);
     }
   } catch (err) {
     console.error("Failed to send offline log:", err);
