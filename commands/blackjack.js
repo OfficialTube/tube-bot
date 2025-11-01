5362edd384919037ddf0e2f8dfb1ab9728837002
Tube <52958347+OfficialTube@users.noreply.github.com>
2025-11-01 16:10:00 -0400

    try this again

--- commands/blackjack.js
+++ commands/blackjack.js
@@ -262,7 +262,7 @@ module.exports = {
                             new ButtonBuilder().setCustomId("playAgain").setLabel("Play Again").setStyle(ButtonStyle.Primary)
                         );
 
-                    await interaction.followUp({
+                    await interaction.editReply({
                         embeds: [new EmbedBuilder()
                             .setTitle("🏁 Results")
                             .setDescription(
